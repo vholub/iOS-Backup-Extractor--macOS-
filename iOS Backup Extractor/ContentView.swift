978f57feb89b0b081c9c045589132861f26a0a86
vholub <vratislavholub@gmail.com>
2024-08-29 23:21:07 +0200

    Initial Commit

---
+++ iOS Backup Extractor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOS Backup Extractor
+//
+//  Created by Vratislav Holub on 29.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS Backup Extractor/iOS_Backup_ExtractorApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Backup_ExtractorApp.swift
+//  iOS Backup Extractor
+//
+//  Created by Vratislav Holub on 29.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Backup_ExtractorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
